db473bc91778285e0437d6c71762554a1bbd0a54
Manish Kumar Sharma <manishsharmadeveloper@gmail.com>
2026-05-02 02:59:41 +0530

    Final Deploy Version 1.9

--- admin/semesters/add.php
+++ admin/semesters/add.php
@@ -12,6 +12,17 @@ if(isset($_POST['save'])){
 
 <form method="POST">
     <label>Semester Allotment</label>
-    <input type="number" name="semester" placeholder="Enter Semester">
+    <!-- <input type="text" name="semester" placeholder="Enter Semester"> -->
+     <select name="semester" required>
+        <option value="Select Semester" selected>Select Semester</option>
+        <option value="Semester 1">Semester 1</option>
+        <option value="Semester 2">Semester 2</option>
+        <option value="Semester 3">Semester 3</option>
+        <option value="Semester 4">Semester 4</option>
+        <option value="Semester 5">Semester 5</option>
+        <option value="Semester 6">Semester 6</option>
+        <option value="Semester 7">Semester 7</option>
+        <option value="Semester 8">Semester 8</option>
+     </select>
     <button name="save">Add</button>
 </form>
